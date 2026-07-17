ca2b6857c2fa429ff6a924c0077591fabc6c7d62
vugarjs <mlolvuqar@gmail.com>
2026-07-17 10:31:03 +0400

    Added some for loops.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿#region 3 ve 5 qaliqsiz bolunen ededler.
+//int total = 0;
+//for (int i = 1; i < 50; i++)
+//{
+//    if(i % 2 == 0)  //3 ve 5 qaliqsiz bolunen ededler.
+//    {
+//        total += i;
+//    }
+//}
+//Console.WriteLine(total);
+#endregion
+#region fizzbuzz
+// 3 e bolunurse fizz 5 be bolunurse bazz ikisinede bolunse fizbuzz
+//for (int i = 0; i <= 50; i++)
+//{
+//    if (i % 3 == 0)
+//    {
+//        Console.WriteLine("Fizz");
+//    }
+//    if (i % 5 == 0)
+//    {
+//        Console.WriteLine("Bazz");
+//    }
+//    if (i % 5 == 0 && i % 3 == 0)
+//    {
+//        Console.WriteLine("fizbazz");
+//    }
+//}
+#endregion
+#region 1-dən 100-ə qədər olan tək ədədləri çap et.
+
+//for (int i = 0; i <100; i++)
+//{
+//    if (i%2==1)
+//        {
+//        Console.WriteLine(i);
+//    }
+//}
+#endregion
+//// 1-dən 50-yə qədər olan 4-ə bölünən ədədləri çap et.
+//for (byte i = 1; i < 50; i++)
+//{
+//    if(i%4==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+
+//}
